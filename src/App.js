9a4d966a465fbb867aa0ff8ad2a1deea8810e772
KotatuBot <kotatubot@gmail.com>
2019-08-31 10:45:19 +0900

    learn props

--- src/App.js
+++ src/App.js
@@ -1,36 +1,53 @@
 import React from 'react';
 
-/*
-function App() {
-  return(
-
-          <div>
-                <label htmlFor="bar">bar </label>
-                <input type="text"onChange={()=>console.log("I am")}/>
-          </div>
-
-  
-  );
-}
-
-*/
-
 function App(){
 
+        const profiles = [
+                {
+                        name:"Taro",
+
+                        age: 10
+                
+                },
+                {
+                        name:"Hanako",
+                        age: 5
+                
+                },
+
+                {
+                
+                        name: "NoName"
+                }
+        
+        ]
         return (
                 <div>
 
-                <Cat/>
-                <Cat/>
+                {
+                
+                        profiles.map((profile,index)=>{
+                        
+                        return <User name={profile.name} age={profile.age} key={index}/> 
+                        
+                        })
+                }
+
+
 
                 </div>
         );
 
 
 }
-function Cat(){
 
-        return (<div>Meow!</div>);
+
+function User(props){
+
+        return (<div>Hi, I am {props.name},and {props.age} years old!</div>);
 }
 
+User.defaultProps = {
+        age:1
+}
 export default App;
